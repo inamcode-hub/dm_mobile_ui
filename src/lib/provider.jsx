a16fca27b5376b2d31602d4a33564d20b113f119
Inam Ul Rehman <inam@dryermaster.com>
2023-11-29 10:28:20 -0500

    theme added

--- src/lib/provider.jsx
+++ src/lib/provider.jsx
@@ -1,14 +1,8 @@
 import { Provider as ReduxProvider } from 'react-redux';
 import Store from '../store';
-import { CssBaseline } from '@mui/material';
 
 const Provider = ({ children }) => {
-  return (
-    <ReduxProvider store={Store}>
-      <CssBaseline enableColorScheme />
-      {children}
-    </ReduxProvider>
-  );
+  return <ReduxProvider store={Store}>{children}</ReduxProvider>;
 };
 
 export default Provider;
---
+++ src/styles/theme.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark', // Set the mode to 'dark' for a dark theme
+    primary: {
+      main: '#ff4400',
+    },
+    secondary: {
+      main: '#0044ff',
+    },
+  },
+});
+
+export default theme;
