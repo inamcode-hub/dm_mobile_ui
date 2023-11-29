8bb450a729f86e189efbc2bd2b38ea9110155f3b
Inam Ul Rehman <inam@dryermaster.com>
2023-11-29 15:18:38 -0500

    login UI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          dependencies: [
+            '@emotion/react',
+            '@emotion/styled',
+            '@fontsource/roboto',
+            '@mui/icons-material',
+            '@mui/material',
+            '@reduxjs/toolkit',
+            'axios',
+            'react',
+            'react-dom',
+            'react-redux',
+            'react-router-dom',
+          ],
+        },
+      },
+    },
+  },
+});
