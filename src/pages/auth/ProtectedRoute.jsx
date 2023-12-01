b21210279d0ca9bdabe2075568f5e78b3a747420
Inam Ul Rehman <inam@dryermaster.com>
2023-12-01 16:27:20 -0500

    update

--- src/pages/auth/ProtectedRoute.jsx
+++ src/pages/auth/ProtectedRoute.jsx
@@ -1,20 +1,8 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import { getStateValues } from '../../features/user/userSlice';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const dispatch = useDispatch();
   const { isMember } = useSelector((state) => state.user);
-  const location = useLocation();
-
-  //   useEffect(() => {
-  //     const token = Cookies.get('dryermaster_token');
-  //     if (!token) {
-  //       dispatch(getStateValues({ name: 'isMember', value: false }));
-  //     }
-  //   }, [location.pathname]);
 
   if (isMember) {
     return <Navigate to={'/dashboard'} />;
