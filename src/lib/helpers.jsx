3da85f376fd78a455b849d6962092f96393551c9
Inam Ul Rehman <inam@dryermaster.com>
2023-12-05 08:50:30 -0500

    update

--- src/lib/helpers.jsx
+++ src/lib/helpers.jsx
@@ -3,9 +3,6 @@
 // =================== Utility functions ===================
 // Utility function to capitalize a string
 export const capitalize = (str) => {
-  console.log('str', str);
   if (typeof str !== 'string' || !str) return '';
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
-
-// =================== Cookie functions ===================
