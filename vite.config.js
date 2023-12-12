c61729718b27fce7c901f85f42943b9eaaefd898
Inam Ul Rehman <inam@dryermaster.com>
2023-12-12 11:32:17 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: '/manifest.json',
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
