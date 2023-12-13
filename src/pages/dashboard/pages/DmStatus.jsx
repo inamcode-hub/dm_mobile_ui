5743c05346d0a1b640f37885f8a5210d60305dc8
Inam Ul Rehman <inam@dryermaster.com>
2023-12-13 16:45:20 -0500

    update

--- src/pages/dashboard/pages/DmStatus.jsx
+++ src/pages/dashboard/pages/DmStatus.jsx
@@ -43,8 +43,12 @@ const DmStatus = () => {
           </span>
           <p>
             If you are still having issues after watching the video, please
-            contact us at
-            <a href='tel:1-800-265-8521'>1-800-265-8521</a>
+            contact us at{' '}
+            <a
+              href='tel:1-800-265-8521'
+              style={{ whiteSpace: 'nowrap' }}>
+              1-800-265-8521
+            </a>
           </p>
         </div>
         <Divider />
