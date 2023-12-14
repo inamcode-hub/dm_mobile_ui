6e94d94ef5d36680b9466d8c08eb30ccfcca056b
Inam Ul Rehman <inam@dryermaster.com>
2023-12-14 13:08:08 -0500

    update

--- src/lib/handleGlobalError.jsx
+++ src/lib/handleGlobalError.jsx
@@ -12,7 +12,7 @@ const CustomToast = ({ header, message }) => (
 export const handleGlobalError = (error, thunkAPI) => {
   let header = '';
   let message = '';
-
+  toast.dismiss();
   if (!error.response) {
     header = 'Network Error';
     message = 'Unable to reach the server';
