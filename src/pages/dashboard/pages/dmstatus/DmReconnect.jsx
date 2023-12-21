f084cf29b264533b2a3eab1af12aebe843626f98
Inam Ul Rehman <inam@dryermaster.com>
2023-12-21 11:25:44 -0500

    update

--- src/pages/dashboard/pages/dmstatus/DmReconnect.jsx
+++ src/pages/dashboard/pages/dmstatus/DmReconnect.jsx
@@ -6,65 +6,68 @@ import WifiIcon from '@mui/icons-material/Wifi';
 import SpeedIcon from '@mui/icons-material/Speed';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import CardWrapper from '../../../../styles/warppers/CardWrapper';
 
 const DmReconnect = () => {
   return (
     <Wrapper>
-      <div className='body'>
-        <Typography
-          variant='h4'
-          component='h2'>
-          <WifiOffIcon sx={{ fontSize: 35, marginRight: '.5rem' }} />
-          Connection Issue
-        </Typography>
-        <Typography variant='body1'>
-          We&apos;re having trouble connecting to your DryerMaster device.
-          Please check the following:
-        </Typography>
-        <ul>
-          <li>
-            <WifiIcon sx={{ verticalAlign: 'middle', marginRight: '.5rem' }} />
-            Ensure your WiFi is on and connected to the internet.
-          </li>
-          <li>
-            <SpeedIcon sx={{ verticalAlign: 'middle', marginRight: '.5rem' }} />
-            Check if your internet connection is stable and strong.
-          </li>
-          <li>
-            <RestartAltIcon
-              sx={{ verticalAlign: 'middle', marginRight: '.5rem' }}
-            />
-            Restart your DryerMaster device and router if necessary.
-          </li>
-          <li>
-            <ContactSupportIcon
-              sx={{ verticalAlign: 'middle', marginRight: '.5rem' }}
-            />
-            If your connection issues persist, please contact our support team
-            for assistance.
-          </li>
-        </ul>
-        <Button
-          variant='contained'
-          size='large'
-          startIcon={<AutorenewIcon />}>
-          Reconnect
-        </Button>
-      </div>
+      <CardWrapper>
+        <div className='body'>
+          <Typography
+            variant='h4'
+            component='h2'>
+            <WifiOffIcon sx={{ fontSize: 35, marginRight: '.5rem' }} />
+            Connection Issue
+          </Typography>
+          <Typography variant='body1'>
+            We&apos;re having trouble connecting to your DryerMaster device.
+            Please check the following:
+          </Typography>
+          <ul>
+            <li>
+              <WifiIcon
+                sx={{ verticalAlign: 'middle', marginRight: '.5rem' }}
+              />
+              Ensure your WiFi is on and connected to the internet.
+            </li>
+            <li>
+              <SpeedIcon
+                sx={{ verticalAlign: 'middle', marginRight: '.5rem' }}
+              />
+              Check if your internet connection is stable and strong.
+            </li>
+            <li>
+              <RestartAltIcon
+                sx={{ verticalAlign: 'middle', marginRight: '.5rem' }}
+              />
+              Restart your DryerMaster device and router if necessary.
+            </li>
+            <li>
+              <ContactSupportIcon
+                sx={{ verticalAlign: 'middle', marginRight: '.5rem' }}
+              />
+              If your connection issues persist, please contact our support team
+              for assistance.
+            </li>
+          </ul>
+          <Button
+            variant='contained'
+            size='large'
+            startIcon={<AutorenewIcon />}>
+            Reconnect
+          </Button>
+        </div>
+      </CardWrapper>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  padding: ${({ theme }) => theme.spacing(2)};
-  margin: 1rem;
-  border-radius: 10px;
-  box-shadow: ${({ theme }) => theme.shadows[2]};
-  background-color: ${({ theme }) => theme.palette.background.paper};
-  max-width: 600px;
-  @media (min-width: 600px) {
-    margin: 3rem auto;
-  }
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   h2 {
     font-weight: 500;
     display: flex;
---
+++ src/styles/warppers/CardWrapper.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+const CardWrapper = styled.div`
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  box-shadow: rgba(145, 158, 171, 0.2) 0px 0px 2px 0px,
+    rgba(145, 158, 171, 0.12) 0px 12px 24px -4px;
+  border-radius: 16px;
+  padding: 40px 24px;
+  z-index: 0;
+  background-color: ${({ theme }) =>
+    theme.palette.mode === 'dark' ? '#333' : 'white'};
+  border: ${({ theme }) => theme.palette.mode === 'dark' && '1px solid #333'};
+
+  @media (max-width: 600px) {
+    width: 90%;
+  }
+  @media (min-width: 600px) {
+    width: 500px;
+  }
+`;
+
+export default CardWrapper;
