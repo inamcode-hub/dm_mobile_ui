4cdf97158680da527bf928c4c13dff47509718ee
Inam Ul Rehman <inam@dryermaster.com>
2024-01-04 16:44:11 -0500

    update

--- src/pages/dashboard/pages/account/Subscription.jsx
+++ src/pages/dashboard/pages/account/Subscription.jsx
@@ -25,6 +25,15 @@ const Subscription = () => {
       <div className='card'>
         <CardWrapper>
           <div className='title'>Subscription</div>
+          <div className='subscription'>
+            <strong>
+              {isSubscriptionActive
+                ? `Your subscription expires on `
+                : `Your subscription expired on `}
+              {date}
+            </strong>
+          </div>
+
           <div className='card-heading'>
             <div className='card-title'>
               <h3>
@@ -38,15 +47,12 @@ const Subscription = () => {
                 <small className='expired'>Expired</small>
               )}
             </div>
-            <strong>
-              {isSubscriptionActive
-                ? `Your subscription expires on `
-                : `Your subscription expired on `}
-              {date}
-            </strong>
+            <Divider />
+
             <div className='card-heading-price'>
-              100 USD<span>/year</span>
+              100 USD<span>/ year</span>
             </div>
+
             <small>New customers get 1 year free trial</small>
           </div>
           <Divider />
@@ -79,83 +85,80 @@ const Wrapper = styled.div`
     max-width: 500px;
     margin: 3rem auto;
     justify-content: center;
-    
-    }
+  }
+  .subscription {
+    text-align: center;
     strong {
       color: ${(props) =>
         props.$isSubscriptionActive ? green[500] : blue[500]};
-      font-size: 0.9rem;
+      font-size: 1.5;
+      font-weight: 500;
+      display: block;
     }
-    .card-heading {
-      border-radius: 10px 10px 0 0;
-      .card-title {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        small {
-          font-size: 0.85rem;
-          font-weight: 400;
-          padding: 0.25rem 0.5rem;
-          border-radius: 5px;
-          &.active {
-            background-color: ${green[500]};
-            color: #fff;
-          }
-          &.expired {
-            background-color: ${blue[500]};
-            color: #fff;
-          }
-        }
-      }
-      h3 {
-        margin: 0;
-        display: flex;
-        align-items: center;
-        font-size: 1.6rem;
-        font-weight: 500;
-        background-color: ${({ theme }) =>
-          theme.palette.mode === 'dark'
-            ? 'var(--primary-16)'
-            : 'var(--primary-2)'};
-        color: ${({ theme }) =>
-          theme.palette.mode === 'dark'
-            ? 'var(--primary-2)'
-            : 'var(--primary-text)'};
-        padding: 0 0.5rem;
+  }
+  .card-heading {
+    border-radius: 10px 10px 0 0;
+    .card-title {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      small {
+        font-size: 0.85rem;
+        font-weight: 400;
+        padding: 0.25rem 0.5rem;
         border-radius: 5px;
-        margin-bottom: 0.5rem;
+        &.active {
+          background-color: ${green[800]};
+          color: #fff;
+        }
+        &.expired {
+          background-color: ${blue[500]};
+          color: #fff;
+        }
       }
-      .card-heading-price {
-        font-size: 1.5rem;
+    }
+    h3 {
+      margin: 0;
+      display: flex;
+      align-items: center;
+      font-size: 1.6rem;
+      font-weight: 500;
+
+      color: ${green[800]};
+      padding: 0 0.5rem;
+      border-radius: 5px;
+      margin-bottom: 0.5rem;
+    }
+    .card-heading-price {
+      font-size: 1.9rem;
+      font-weight: 500;
+      span {
+        font-size: 0.85rem;
+        margin-left: 0.25rem;
         font-weight: 500;
-        span {
-          font-size: 0.85rem;
-          margin-left: 0.25rem;
-          font-weight: 500;
+      }
+    }
+  }
+  .card-body {
+    padding: 1rem 0;
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      li {
+        padding: 0.5rem 0;
+        display: flex;
+        align-items: center;
+        svg {
+          margin-right: 0.5rem;
+          color: ${green[500]};
         }
       }
     }
-    .card-body {
-      padding: 1rem 0;
-      ul {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        li {
-          padding: 0.5rem 0;
-          display: flex;
-          align-items: center;
-          svg {
-            margin-right: 0.5rem;
-            color: ${green[500]};
-          }
-        }
-      }
-    }
-    .card-footer {
-      button {
-        width: 100%;
-      }
+  }
+  .card-footer {
+    button {
+      width: 100%;
     }
   }
 `;
