17dc14fcdbe2c1eb3583da3b415e67889fef8cf2
Inam Ul Rehman <inam@dryermaster.com>
2024-01-29 13:33:44 -0500

    update

--- src/pages/dashboard/pages/home/Home.jsx
+++ src/pages/dashboard/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 
 import DmStatus from '../dmstatus/DmStatus';
 import Cards from './components/cards/Cards';
+import Chart from './components/chart/Chart';
+import Controller from './components/control/Controller';
 
 const Home = () => {
   const { isDmOnline, isSubscriptionActive } = useSelector(
@@ -14,6 +16,8 @@ const Home = () => {
   return (
     <Wrapper>
       <Cards />
+      <Chart />
+      <Controller />
     </Wrapper>
   );
 };
