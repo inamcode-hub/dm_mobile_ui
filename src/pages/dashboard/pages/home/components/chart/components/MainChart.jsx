213cc02e0311c82f4e78a990273993ac5e2d69e4
Inam Ul Rehman <inam@dryermaster.com>
2024-01-29 15:20:24 -0500

    update

--- src/pages/dashboard/pages/home/components/chart/components/MainChart.jsx
+++ src/pages/dashboard/pages/home/components/chart/components/MainChart.jsx
@@ -12,6 +12,7 @@ import {
 } from 'recharts';
 import { data } from './Data';
 import { useWindowSize } from '../../../../../../../hooks/useWindowSize';
+import styled from '@emotion/styled';
 
 const MainChart = () => {
   const [chartData, setChartData] = useState([]);
@@ -21,65 +22,58 @@ const MainChart = () => {
   useEffect(() => {
     const formattedData = data.map((item) => ({
       ...item,
-      inlet: item.inlet.toString().slice(0, 2),
-      outlet: item.outlet.toString().slice(0, 2),
-      rate: item.rate.toString().slice(0, 1),
-      target: item.target.toString().slice(0, 2),
+      inlet: Math.round(item.inlet),
+      target: Math.round(item.target),
+      outlet: Math.round(item.outlet),
 
       createdAt: format(new Date(item.createdAt), 'EEE hh:mm'), // Format date using date-fns
     }));
     setChartData(formattedData);
   }, []);
 
-  // Calculate the max and min values for the Y-axis
-  const maxYValue = Math.max(
-    ...data.map((item) =>
-      Math.max(item.inlet, item.outlet, item.rate, item.target)
-    )
-  );
-  const minYValue = Math.min(
-    ...data.map((item) =>
-      Math.min(item.inlet, item.outlet, item.rate, item.target)
-    )
-  );
+  const tickFormatter = (tickItem) => {
+    return Math.round(tickItem);
+  };
 
   return (
-    <ResponsiveContainer
-      width='100%'
-      height={400}>
-      <LineChart
-        data={chartData}
-        margin={{ top: 5, right: 30, left: 0, bottom: 5 }}>
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='createdAt' />
-        <YAxis domain={[minYValue, maxYValue]} />
-        <Tooltip formatter={(value, name) => [value, name]} />
-        <Legend />
-        <Line
-          type='monotone'
-          dataKey='inlet'
-          stroke='#5cb85c'
-          dot={false}
-          label={isMobile ? false : { position: 'top' }}
-        />
-
-        <Line
-          type='monotone'
-          dataKey='target'
-          stroke='#333'
-          dot={false}
-          label={isMobile ? false : { position: 'top' }}
-        />
-        <Line
-          type='monotone'
-          dataKey='outlet'
-          stroke='#428bca'
-          dot={false}
-          label={isMobile ? false : { position: 'top' }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <Wrapper>
+      <ResponsiveContainer
+        width='100%'
+        height={400}>
+        <LineChart
+          data={chartData}
+          margin={{ top: 5, right: 30, left: -20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='createdAt' />
+          <YAxis tickFormatter={tickFormatter} />
+          <Tooltip formatter={(value, name) => [value, name]} />
+          <Legend />
+          <Line
+            type='monotone'
+            dataKey='inlet'
+            stroke='#5cb85c'
+            dot={false}
+            label={isMobile ? false : { position: 'top' }}
+          />
+          <Line
+            type='monotone'
+            dataKey='target'
+            stroke='#333'
+            dot={false}
+            label={isMobile ? false : { position: 'top' }}
+          />
+          <Line
+            type='monotone'
+            dataKey='outlet'
+            stroke='#428bca'
+            dot={false}
+            label={isMobile ? false : { position: 'top' }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.div``;
 export default MainChart;
