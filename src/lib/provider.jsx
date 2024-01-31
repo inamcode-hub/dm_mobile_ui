52e627059b7992a4cd3c165a42fd1c83a5648e59
Inam Ul Rehman <inam@dryermaster.com>
2024-01-31 16:12:16 -0500

    update

--- src/lib/provider.jsx
+++ src/lib/provider.jsx
@@ -1,9 +1,7 @@
 import { Provider as ReduxProvider } from 'react-redux';
 import Store from '../store';
-import ThemeChecker from '../components/ThemeChecker';
 
 const Provider = ({ children }) => {
-  <ThemeChecker />;
   return <ReduxProvider store={Store}>{children}</ReduxProvider>;
 };
 
