28d6aeaf685a413c3cda95eb26d09c6466d169af
Inam Ul Rehman <inam@dryermaster.com>
2024-02-06 10:06:35 -0500

    update

--- src/pages/dashboard/pages/home/components/chart/Chart.jsx
+++ src/pages/dashboard/pages/home/components/chart/Chart.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import MainChart from './components/MainChart';
 import ControlChart from './components/ControlChart';
 import styled from '@emotion/styled';
+import ChartData from './components/ChartData';
 
 const Chart = () => {
   return (
     <Wrapper>
-      <MainChart />
-      <ControlChart />
+      {/* <MainChart />
+      <ControlChart /> */}
+      <ChartData />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  gap: 1rem;
+  /* display: grid;
+  gap: 1rem; */
 `;
 export default Chart;
