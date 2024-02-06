3c0431b009fa7a145bde222db529e1921a954c59
Inam Ul Rehman <inam@dryermaster.com>
2024-02-06 10:57:04 -0500

    update

--- src/pages/dashboard/pages/home/components/chart/components/ChartData.jsx
+++ src/pages/dashboard/pages/home/components/chart/components/ChartData.jsx
@@ -9,9 +9,6 @@ import { useWindowSize } from '../../../../../../../hooks/useWindowSize';
 
 const ChartData = () => {
   const { width } = useWindowSize();
-
-  const theme = useTheme();
-  const isDarkMode = theme.palette.mode === 'dark';
   const seriesInlet = data.map((item) => {
     return {
       x: format(new Date(item.createdAt), 'EEE hh:mm'),
@@ -51,7 +48,7 @@ const ChartData = () => {
         show: true,
       },
       zoom: {
-        enabled: true,
+        enabled: false,
       },
 
       dropShadow: {
@@ -63,26 +60,19 @@ const ChartData = () => {
         opacity: 0.2,
       },
     },
-    colors: isDarkMode
-      ? ['#5cb85c', '#353535', '#0961ad']
-      : ['#5cb85c', '#000', '#0961ad'],
+    colors: ['#5cb85c', '#000', '#0961ad'],
     stroke: {
       curve: 'smooth',
     },
     title: {
       text: 'Average High & Low Moisture',
       align: 'left',
-      style: {
-        color: '#000', // Change title color based on theme
-      },
     },
     xaxis: {
       type: 'category',
-
       tickAmount:
         // width bigger than 768px than 10 if screen is bigger than 920px than 20
         width > 768 ? (width > 920 ? 15 : 8) : 7,
-
       labels: {
         style: {
           colors: '#000',
@@ -136,6 +126,25 @@ const ChartData = () => {
     stroke: {
       curve: 'smooth',
     },
+    title: {
+      text: 'Average High & Low Rate',
+      align: 'left',
+    },
+    xaxis: {
+      type: 'category',
+      tickAmount:
+        // width bigger than 768px than 10 if screen is bigger than 920px than 20
+        width > 768 ? (width > 920 ? 15 : 8) : 7,
+      labels: {
+        formatter: function (val) {
+          return val;
+        },
+      },
+    },
+    legend: {
+      show: true,
+      showForSingleSeries: true, // Add this line
+    },
   });
 
   const [series2, setSeries2] = useState([
@@ -145,7 +154,7 @@ const ChartData = () => {
     },
   ]);
 
-  useEffect(() => {}, [isDarkMode]);
+  useEffect(() => {}, []);
   return (
     <Wrapper>
       <div
@@ -176,20 +185,49 @@ const Wrapper = styled.div`
   display: grid;
   gap: 1rem;
   overflow: hidden;
-  @media (min-width: 768px) {
-    .apexcharts-toolbar {
-      margin-right: 1rem;
-    }
-    //tooltip
+  margin-bottom: 5rem;
+  .apexcharts-toolbar {
+    margin-right: 1rem;
+  }
+
+  .apexcharts-title-text {
+    fill: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#fff' : '#000'} !important;
+    text-transform: uppercase;
+  }
+  .apexcharts-xaxis-label,
+  .apexcharts-yaxis-label {
+    fill: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#fff' : '#000'} !important;
+  }
+
+  .apexcharts-tooltip {
+    background: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#333' : '#fff'} !important;
+    color: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#fff' : '#000'} !important;
+  }
+  .apexcharts-tooltip-title {
+    background: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#000' : ''} !important;
+    color: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#fff' : '#000'} !important;
   }
   .apexcharts-legend-text {
     color: ${({ theme }) =>
       theme.palette.mode === 'dark' ? '#fff' : '#000'} !important;
+    text-transform: capitalize;
   }
 
   .chart-line,
   .chart-line2 {
     overflow: hidden;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    background-color: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? '#000' : '#fff'};
+
+    margin: 0 1rem;
   }
   .chart-line {
   }
