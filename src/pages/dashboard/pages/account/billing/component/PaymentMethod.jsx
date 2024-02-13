5fa20732471c5e9457cd717cb0e5c7481c48e326
Inam Ul Rehman <inam@dryermaster.com>
2024-02-13 10:43:15 -0500

    update

--- src/pages/dashboard/pages/account/billing/component/PaymentMethod.jsx
+++ src/pages/dashboard/pages/account/billing/component/PaymentMethod.jsx
@@ -10,42 +10,57 @@ const PaymentMethod = () => {
   );
   const dispatch = useDispatch();
 
-  const handleCardChange = (e) => {
-    dispatch(getUserAccountStateValues({ name: 'showNewCard', value: e }));
+  const handleCardChange = (showNewCard) => {
+    dispatch(
+      getUserAccountStateValues({ name: 'showNewCard', value: showNewCard })
+    );
   };
+
   useEffect(() => {
-    if (paymentCards.length === 0) {
-      dispatch(getUserAccountStateValues({ name: 'showNewCard', value: true }));
-    } else {
-      dispatch(
-        getUserAccountStateValues({ name: 'showNewCard', value: false })
-      );
-    }
-  }, [paymentCards]);
+    dispatch(
+      getUserAccountStateValues({
+        name: 'showNewCard',
+        value: paymentCards.length === 0,
+      })
+    );
+  }, [paymentCards, dispatch]);
+
   return (
     <Wrapper>
-      <div className='heading'>
-        <div className='title'>Renew Subscription</div>
-        <div className='subscription_information'></div>
-        <div className='options'>
-          <ButtonGroup>
-            <Button
-              variant={showNewCard ? 'contained' : 'outlined'}
-              color='primary'
-              onClick={() => handleCardChange(true)}>
-              New Card
-            </Button>
-            <Button
-              variant={!showNewCard ? 'contained' : 'outlined'}
-              color='primary'
-              onClick={() => handleCardChange(false)}
-              disabled={paymentCards.length === 0}>
-              Existing Cards
-            </Button>
-          </ButtonGroup>
-        </div>
-      </div>
-      <div className='subscription_information'></div>
+      <Heading>
+        <Title>Renew Subscription</Title>
+        <SubscriptionInfo>
+          <InfoItem>
+            Amount: <strong>$100.00 USD</strong>
+          </InfoItem>
+          <InfoItem>
+            Frequency: <strong>Yearly</strong>
+          </InfoItem>
+          <InfoItem>
+            Expiry Date: <strong>12/12/2022</strong>
+          </InfoItem>
+          <InfoItem>
+            Renewal Date: <strong>12/12/2022</strong>
+          </InfoItem>
+        </SubscriptionInfo>
+      </Heading>
+      <Options>
+        <StyledButtonGroup
+          variant='contained'
+          color='primary'>
+          <Button
+            variant={showNewCard ? 'contained' : 'outlined'}
+            onClick={() => handleCardChange(true)}>
+            New Card
+          </Button>
+          <Button
+            variant={!showNewCard ? 'contained' : 'outlined'}
+            onClick={() => handleCardChange(false)}
+            disabled={paymentCards.length === 0}>
+            Existing Cards
+          </Button>
+        </StyledButtonGroup>
+      </Options>
     </Wrapper>
   );
 };
@@ -54,9 +69,44 @@ const Wrapper = styled.div`
   padding: 1rem;
   border-bottom: 1px solid #d9d9d9;
   background-color: ${({ theme }) => theme.palette.background.paper};
-  .title {
-    font-size: 1.5rem;
-    font-weight: 500;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Heading = styled.div``;
+
+const Title = styled.div`
+  font-size: 2rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+`;
+
+const SubscriptionInfo = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const InfoItem = styled.div`
+  font-size: 1rem;
+  margin: 0.25rem 0;
+  border-bottom: 1px solid #d9d9d9;
+  padding-bottom: 0.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  max-width: 400px;
+`;
+
+const Options = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledButtonGroup = styled(ButtonGroup)`
+  & .MuiButton-root {
+    /* margin-right: 10px; */
+    width: 150px;
   }
 `;
+
 export default PaymentMethod;
